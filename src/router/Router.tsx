82b655d5b8669a0543e8ec6f6b7ad1ad28ed5e3c
ArtemPylhun <artem.pylhun@oa.edu.ua>
2025-02-04 18:17:35 +0200

    feat: httpClient added

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/layout/Layout";
 import NotFoundPage from "../components/common/NotFoundPage";
 import Login from "../features/auth/Login";
 import Register from "../features/auth/Register";
+import ProtectedRoute from "./ProtectedRoute";
 
 const Router = () => {
   return (
@@ -11,7 +12,16 @@ const Router = () => {
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute allowedRoles={["Admin"]}>
+                <h1>Users</h1>
+              </ProtectedRoute>
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
