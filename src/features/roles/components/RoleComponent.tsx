a70614e7fcc55cf400eed56b26b326f365e6cf84
ArtemPylhun <artem.pylhun@oa.edu.ua>
2025-02-14 23:38:55 +0200

    refactor: roles table refactored, state moved to component

--- src/features/roles/components/RoleComponent.tsx
+++ src/features/roles/components/RoleComponent.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { Input, Form, Select, Button } from "antd";
+import CustomModal from "../../../components/common/CustomModal";
 import SearchInput from "../../../components/common/SearchInput";
 import LoaderComponent from "../../../components/common/Loader";
 import RolesTable from "./table/RolesTable";
+import { PlusOutlined } from "@ant-design/icons";
 import useRoles from "../hooks/useRoles";
-
-const UserComponent = () => {
+import useRoleModal from "../hooks/useRoleModal";
+import "./table/RolesTable.css";
+const RoleComponent = () => {
   const {
     roles,
     roleGroups,
@@ -13,6 +17,17 @@ const UserComponent = () => {
     handleUpdateRole,
     handleDeleteRole,
   } = useRoles();
+
+  const {
+    modalMode,
+    isModalVisible,
+    selectedRole,
+    newRole,
+    showModal,
+    hideModal,
+    setNewRole,
+    setSelectedRole,
+  } = useRoleModal();
   const [filterQuery, setFilterQuery] = useState<string>("");
 
   const handleFilterQueryChange = (
@@ -25,22 +40,131 @@ const UserComponent = () => {
     return role.name.toLowerCase().includes(filterQuery.toLowerCase());
   });
 
+  const handleSave = useCallback(async () => {
+    if (!modalMode) return;
+
+    let result = false;
+
+    if (modalMode === "create" && newRole) {
+      result = await handleCreateRole(newRole);
+    } else if (modalMode === "update" && selectedRole) {
+      result = await handleUpdateRole(selectedRole);
+    } else if (modalMode === "delete" && selectedRole) {
+      result = await handleDeleteRole(selectedRole.id);
+    }
+    if (result) hideModal();
+  }, [
+    modalMode,
+    newRole,
+    selectedRole,
+    handleCreateRole,
+    handleUpdateRole,
+    handleDeleteRole,
+    hideModal,
+  ]);
+
   return (
     <div>
-      <SearchInput
-        query={filterQuery}
-        onQueryChange={handleFilterQueryChange}
-      />
+      <div className="projects-header">
+        <SearchInput
+          query={filterQuery}
+          onQueryChange={handleFilterQueryChange}
+        />
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => showModal(null, "create")}
+          style={{ height: "40px", display: "flex", alignItems: "center" }}
+        >
+          Create Role
+        </Button>
+      </div>
+
       <LoaderComponent loading={loading}>
         <RolesTable
           roles={filteredRoles}
           roleGroups={roleGroups}
-          handleCreateRole={handleCreateRole}
-          handleUpdateRole={handleUpdateRole}
-          handleDeleteRole={handleDeleteRole}
+          showModal={showModal}
         />
       </LoaderComponent>
+
+      <CustomModal
+        visible={isModalVisible}
+        title={
+          modalMode === "create"
+            ? "Create New Role"
+            : modalMode === "update"
+            ? "Update Role Info"
+            : "Delete Role"
+        }
+        onOk={handleSave}
+        onCancel={hideModal}
+      >
+        {(modalMode === "create" || modalMode === "update") && (
+          <Form layout="vertical">
+            <Form.Item label="Name" required>
+              <Input
+                placeholder="Name"
+                value={
+                  (modalMode === "create"
+                    ? newRole?.name
+                    : selectedRole?.name) || ""
+                }
+                onChange={(e) =>
+                  modalMode === "create"
+                    ? setNewRole((prev) => ({
+                        ...prev!,
+                        name: e.target.value,
+                      }))
+                    : setSelectedRole((prev) => ({
+                        ...prev!,
+                        name: e.target.value,
+                      }))
+                }
+              />
+            </Form.Item>
+
+            <Form.Item label="Role group" required>
+              <Select
+                style={{ width: "100%" }}
+                placeholder="Select Role group"
+                value={
+                  modalMode === "create" ? undefined : selectedRole?.roleGroup
+                }
+                onChange={(value) => {
+                  const selectedGroup = roleGroups?.find(
+                    (group) => group.id === value
+                  );
+                  if (!selectedGroup) return;
+
+                  if (modalMode === "update") {
+                    setSelectedRole((prev) => ({
+                      ...prev!,
+                      roleGroup: selectedGroup.id,
+                    }));
+                  } else {
+                    setNewRole((prev) => ({
+                      ...prev!,
+                      roleGroup: selectedGroup.id,
+                    }));
+                  }
+                }}
+              >
+                {roleGroups?.map((group) => (
+                  <Select.Option key={Math.random()} value={group.id}>
+                    {group.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Form>
+        )}
+
+        {modalMode === "delete" && selectedRole && (
+          <p>Are you sure you want to delete this role?</p>
+        )}
+      </CustomModal>
     </div>
   );
 };
-export default UserComponent;
+export default RoleComponent;
--- src/features/roles/components/table/RolesTable.tsx
+++ src/features/roles/components/table/RolesTable.tsx
@@ -1,48 +1,23 @@
 import React, { useMemo } from "react";
-import {
-  Table,
-  TableColumnsType,
-  Space,
-  Button,
-  Input,
-  Form,
-  Select,
-} from "antd";
-import { EditOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
-import CustomModal from "../../../../components/common/CustomModal";
-import useRoleModal from "../../hooks/useRoleModal";
-import {
-  RoleInterface,
-  RoleCreateInterface,
-} from "../../interfaces/RoleInterface";
+import { Table, TableColumnsType, Space, Button } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { RoleInterface } from "../../interfaces/RoleInterface";
 import { RoleGroupInterface } from "../../interfaces/RoleGroupIntreface";
 
 interface RolesTableProps {
   roles: RoleInterface[] | undefined;
   roleGroups: RoleGroupInterface[] | null;
-  handleCreateRole: (role: RoleCreateInterface) => Promise<boolean>;
-  handleUpdateRole: (role: RoleInterface) => Promise<boolean>;
-  handleDeleteRole: (roleId: string) => Promise<boolean>;
+  showModal: (
+    project: RoleInterface | null,
+    mode: "create" | "update" | "delete"
+  ) => void;
 }
 
 const RolesTable: React.FC<RolesTableProps> = ({
   roles,
   roleGroups,
-  handleCreateRole,
-  handleUpdateRole,
-  handleDeleteRole,
+  showModal,
 }) => {
-  const {
-    modalMode,
-    isModalVisible,
-    selectedRole,
-    newRole,
-    showModal,
-    hideModal,
-    setNewRole,
-    setSelectedRole,
-  } = useRoleModal();
-
   const columns: TableColumnsType<RoleInterface> = useMemo(
     () => [
       {
@@ -85,111 +60,13 @@ const RolesTable: React.FC<RolesTableProps> = ({
 
   return (
     <>
-      <div className="row">
-        <Button
-          className="md-2"
-          type="default"
-          icon={<PlusOutlined />}
-          onClick={() => showModal(null, "create")}
-        >
-          Create Role
-        </Button>
-      </div>
-
-      <Table columns={columns} dataSource={roles} rowKey="id" />
-
-      <CustomModal
-        visible={isModalVisible}
-        title={
-          modalMode === "create"
-            ? "Create New Role"
-            : modalMode === "update"
-            ? "Update Role Info"
-            : "Delete Role"
-        }
-        onOk={async () => {
-          let result = false;
-          switch (modalMode) {
-            case "create":
-              result = await handleCreateRole(newRole!);
-              break;
-            case "update":
-              result = await handleUpdateRole(selectedRole!);
-              break;
-            case "delete":
-              result = await handleDeleteRole(selectedRole!.id);
-              break;
-          }
-          if (result) {
-            hideModal();
-          }
-        }}
-        onCancel={hideModal}
-      >
-        {(modalMode === "create" || modalMode === "update") && (
-          <Form layout="vertical">
-            <Form.Item label="Name" required>
-              <Input
-                placeholder="Name"
-                value={
-                  (modalMode === "create"
-                    ? newRole?.name
-                    : selectedRole?.name) || ""
-                }
-                onChange={(e) =>
-                  modalMode === "create"
-                    ? setNewRole((prev) => ({
-                        ...prev!,
-                        name: e.target.value,
-                      }))
-                    : setSelectedRole((prev) => ({
-                        ...prev!,
-                        name: e.target.value,
-                      }))
-                }
-              />
-            </Form.Item>
-
-            <Form.Item label="Role group" required>
-              <Select
-                style={{ width: "100%" }}
-                placeholder="Select Role group"
-                value={
-                  modalMode === "create" ? undefined : selectedRole?.roleGroup
-                }
-                onChange={(value) => {
-                  const selectedGroup = roleGroups?.find(
-                    (group) => group.id === value
-                  );
-                  if (!selectedGroup) return;
-
-                  if (modalMode === "update") {
-                    setSelectedRole((prev) => ({
-                      ...prev!,
-                      roleGroup: selectedGroup.id,
-                    }));
-                  } else {
-                    setNewRole((prev) => ({
-                      ...prev!,
-                      roleGroup: selectedGroup.id,
-                    }));
-                  }
-                }}
-              >
-                {roleGroups?.map((group) => (
-                  <Select.Option key={Math.random()} value={group.id}>
-                    {group.name}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Form>
-        )}
-
-        {modalMode === "delete" && selectedRole && (
-          <p>Are you sure you want to delete this role?</p>
-        )}
-      </CustomModal>
+      <Table
+        columns={columns}
+        dataSource={roles}
+        rowKey="id"
+        className="modern-table"
+        pagination={{ pageSize: 8 }}
+      />
     </>
   );
 };
